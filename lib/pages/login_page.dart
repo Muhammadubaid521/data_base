9127fd56822b98f1f2d2b5726edd4a7c40605a24
Muhammadubaid521 <ubaidmuhammad521@gmail.com>
2023-08-07 10:43:29 +0500

    newh .h

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -17,118 +17,121 @@ class _LoginPageState extends State<LoginPage> {
     final passwordController = TextEditingController();
     final formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: SafeArea(
-          child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: emailController,
-              validator: (value) {
-                if (value == null) {
-                  return 'Please Enter Email';
-                }
-                if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                    .hasMatch(value)) {
-                  return 'Please a valid Email';
-                } else if (value.trim().isEmpty) {
-                  return 'Please Enter Email';
-                }
-
-                return null;
-              },
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                hintText: "email",
-                filled: true,
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: passwordController,
-              validator: (value) {
-                if (value == null) {
-                  return 'Please a Enter Password';
-                } else if (value.trim().isEmpty) {
-                  return 'Please Enter Password';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                hintText: "password",
-                filled: true,
-              ),
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(300, 60),
-                    backgroundColor: const Color(0xff007074)),
-                onPressed: () async {
-                  bool isFormValid = formKey.currentState!.validate();
-                  if (!isFormValid) {
-                    return;
+              TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Enter Email';
                   }
-                  List<Map<String, dynamic>> items = await DatabaseHelper.get();
+                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                      .hasMatch(value)) {
+                    return 'Please a valid Email';
+                  } else if (value.trim().isEmpty) {
+                    return 'Please Enter Email';
+                  }
 
-                  for (var a in items) {
-                    if (a["email"] == emailController.text &&
-                        a["password"] == passwordController.text) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomePage(),
-                          ));
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintText: "email",
+                  filled: true,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please a Enter Password';
+                  } else if (value.trim().isEmpty) {
+                    return 'Please Enter Password';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintText: "password",
+                  filled: true,
+                ),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(300, 60),
+                      backgroundColor: const Color(0xff007074)),
+                  onPressed: () async {
+                    bool isFormValid = formKey.currentState!.validate();
+                    if (!isFormValid) {
+                      return;
                     }
-                  }
+                    List<Map<String, dynamic>> items =
+                        await DatabaseHelper.get();
 
-                  setState(() {
-                    print("obj $items");
-                  });
-                },
-                child: const Text(
-                  'login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                )),
-            InkWell(
-                onTap: () async {
-                  Navigator.pop(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HomePage(),
-                      ));
-                },
-                child: const Text('signup')),
-          ],
-        ),
-      )),
+                    for (var a in items) {
+                      if (a["email"] == emailController.text &&
+                          a["password"] == passwordController.text) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(),
+                            ));
+                      }
+                    }
+
+                    setState(() {
+                      print("obj $items");
+                    });
+                  },
+                  child: const Text(
+                    'login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )),
+              InkWell(
+                  onTap: () async {
+                    Navigator.pop(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomePage(),
+                        ));
+                  },
+                  child: const Text('signup')),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
--- lib/pages/singup_page.dart
+++ lib/pages/singup_page.dart
@@ -17,145 +17,147 @@ class _SignUppageState extends State<SignUppage> {
     final passwordController = TextEditingController();
     final _formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: SafeArea(
-          child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: nameController,
-              validator: (value) {
-                if (value == null) {
-                  return 'Please Enter Name';
-                } else if (value.trim().isEmpty) {
-                  return 'Please Enter Name';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                hintText: "name",
-                filled: true,
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: emailController,
-              validator: (value) {
-                if (value == null) {
-                  return 'Please Enter Email';
-                }
-                if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                    .hasMatch(value)) {
-                  return 'Please a valid Email';
-                } else if (value.trim().isEmpty) {
-                  return 'Please Enter Email';
-                }
-
-                return null;
-              },
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                hintText: "Enter your Email here",
-                filled: true,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: passwordController,
-              validator: (value) {
-                if (value == null) {
-                  return 'Please a Enter Password';
-                } else if (value.trim().isEmpty) {
-                  return 'Please Enter Password';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                ),
-                hintText: "password",
-                filled: true,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(300, 60),
-                    backgroundColor: const Color(0xff007074)),
-                onPressed: () async {
-                  bool isFormValid = _formKey.currentState!.validate();
-                  if (!isFormValid) {
-                    return;
+              TextFormField(
+                controller: nameController,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Enter Name';
+                  } else if (value.trim().isEmpty) {
+                    return 'Please Enter Name';
                   }
-                  DatabaseHelper.save(nameController.text, emailController.text,
-                      passwordController.text);
-                  //
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                    //
-                  ));
-                  List<Map<String, dynamic>> get = await DatabaseHelper.get();
-
-                  setState(() {
-                    print("get $get");
-                  });
+                  return null;
                 },
-                child: const Text(
-                  'create account',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
                   ),
-                )),
-            InkWell(
-                onTap: () async {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginPage(),
-                      ));
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintText: "name",
+                  filled: true,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Enter Email';
+                  }
+                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                      .hasMatch(value)) {
+                    return 'Please a valid Email';
+                  } else if (value.trim().isEmpty) {
+                    return 'Please Enter Email';
+                  }
+
+                  return null;
                 },
-                child: const Text('login')),
-          ],
-        ),
-      )),
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintText: "Enter your Email here",
+                  filled: true,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please a Enter Password';
+                  } else if (value.trim().isEmpty) {
+                    return 'Please Enter Password';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintText: "password",
+                  filled: true,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(300, 60),
+                      backgroundColor: const Color(0xff007074)),
+                  onPressed: () async {
+                    bool isFormValid = _formKey.currentState!.validate();
+                    if (!isFormValid) {
+                      return;
+                    }
+                    DatabaseHelper.save(nameController.text,
+                        emailController.text, passwordController.text);
+                    //
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                      //
+                    ));
+                    List<Map<String, dynamic>> get = await DatabaseHelper.get();
+
+                    setState(() {
+                      print("get $get");
+                    });
+                  },
+                  child: const Text(
+                    'create account',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )),
+              InkWell(
+                  onTap: () async {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ));
+                  },
+                  child: const Text('login')),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
